9fd09ca1dd640cdab870494439e8420fcc0e9202
joshdei <joshuadeinne@gmail.com>
2025-02-03 16:51:55 +0100

    Update slider.blade.php

--- resources/views/slider.blade.php
+++ resources/views/slider.blade.php
@@ -27,7 +27,7 @@
         <li class="dropdown">
           <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="mail"></i><span>Preview</span></a>
           <ul class="dropdown-menu">
-            <li><a class="nav-link" href="{{route('preview.question')}}">Qestion</a></li>
+            <li><a class="nav-link" href="{{route('preview.question')}}">Question</a></li>
            
           </ul>
         </li>
